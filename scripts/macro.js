af7b8255fa81b99a5b6b4bd95388d15303b3549c
Dylan <rdylanmoore@gmail.com>
2025-11-21 00:23:27 -0600

    Multiacttack and single attack outputs seem to be working now, even if they are a little messy. Need to confirm that all modifiers are still applying but I think this is working

--- scripts/macro.js
+++ scripts/macro.js
@@ -7,23 +7,22 @@ function macroComponents(map, attackNumber) {
   calculateAttack();
 
   // ### Creates the prefix - should only be used once in the combined macro ###
-  let macroPrefix = `&{template:pc} {{type=attackdamage}} {{name=${
+  let macroPrefix = `&{template:pc} {{type=attackdamage}} {{name= ${
     config.macro.attackName
-  }}} {{attack=1}} {{showchar=[[1]]}} {{atkvs=(Melee vs AC)}} {{dmg1flag=1}} ${
+  }  }} {{attack=1}} {{showchar=[[1]]}} {{atkvs=(Melee vs AC)}} {{dmg1flag=1}} ${
     config.macro.damageOther != '' ? '{{dmg2flag=1}} {{dmg2name=Acid}}' : ''
   } {{charname=Lord Guber}}`;
 
-  // implement adding queries later on
-  // let macroQuery = ' + ?{AttackMod|0}'\
+  // this is not dry at all... figure something out
+  const counter = attackNumber > 0 ? `${attackNumber}` : '';
 
-  const rollKey = attackNumber > 0 ? `roll${attackNumber}` : 'roll';
-  let CombinedMacroRoll = `{{${rollKey}=[[1d20cs>${config.weapon.critRange} + ${
-    config.base.bab
-  }[BAB] + ${config.base.strBonus}[Strength] + ${
+  let CombinedMacroRoll = `{{roll${counter}=[[1d20cs>${
+    config.weapon.critRange
+  } + ${config.base.bab}[BAB] + ${config.base.strBonus}[Strength] + ${
     config.macro.attackBonus
   }[Buff] + ${map}[MAP] ${
     config.macro.queryToggle ? ' + ?{AttackMod|0}' : ''
-  }]]}} {{critconfirm=[[1d20 + ${config.base.bab}[BAB] + ${
+  }]]}} {{critconfirm${counter}=[[1d20 + ${config.base.bab}[BAB] + ${
     config.base.strBonus
   }[Strength] + ${config.macro.attackBonus}[Buff]+ ${map}[MAP] ${
     config.macro.queryToggle ? ' + ?{AttackMod|0}' : ''
@@ -41,12 +40,13 @@ function macroComponents(map, attackNumber) {
     config.macro.queryToggle ? ' + ?{AttackMod|0}' : ''
   } ]]}}`;
 
-  const damageKey = attackNumber > 0 ? `${attackNumber}` : 'roll';
-  let macroDamage = `{{rolldmg1=[[${config.weapon.damageDice} ${config.macro.vitalStrikeDamage} + ${config.macro.damageTotal} ]]}} {{rolldmg1type=Slashing}}{{rolldmg1crit=[[(${config.weapon.damageDice} + ${config.macro.damageTotal})*2 ${config.macro.vitalStrikeDamage}]]}}`;
-  let combinedMacroDamage = `{{damage=1}} {{dmg1flag=1}} {{dmg1=[[${config.weapon.damageDice} ${config.macro.vitalStrikeDamage} + ${config.macro.damageTotal} ]]}} {{dmg1type=Slashing}}{{dmg1crit=[[(${config.weapon.damageDice} + ${config.macro.damageTotal})*2 ${config.macro.vitalStrikeDamage}]]}}`;
-  if (config.macro.damageOther != '') {
-    macroDamage += `{{dmg2flag=1}}{{dmg2type=Fire}}{{dmg2=[[${config.macro.damageOther}]]}}{{dmg2crit=[[${config.macro.damageOther}]]}}`;
-  }
+  let macroDamage = `{{damage=1}}{{dmg1flag=1}}{{dmg1=[[${config.weapon.damageDice} ${config.macro.vitalStrikeDamage} + ${config.macro.damageTotal} ]]}} {{dmg1type=Slashing}}{{dmg1crit=[[(${config.weapon.damageDice} + ${config.macro.damageTotal})*2 ${config.macro.vitalStrikeDamage}]]}}`;
+
+  let combinedMacroDamage = `{{roll${counter}dmg1=[[${config.weapon.damageDice} ${config.macro.vitalStrikeDamage} + ${config.macro.damageTotal} ]]}} {{roll${counter}dmg1type=Slashing}}{{roll${counter}dmg1crit=[[(${config.weapon.damageDice} + ${config.macro.damageTotal})*2 ${config.macro.vitalStrikeDamage}]]}}`;
+
+  // if (config.macro.damageOther != '') {
+  //   macroDamage += `{{dmg2flag=1}}{{dmg2type=Fire}}{{dmg2=[[${config.macro.damageOther}]]}}{{dmg2crit=[[${config.macro.damageOther}]]}}`;
+  // }
 
   macroParts = {
     prefix: macroPrefix,
@@ -64,13 +64,14 @@ export function macroBuilder(activeAction, haste) {
 
   // First Attack
   const macro = macroParts.prefix + macroParts.roll + macroParts.damage;
-  let macroRunning = macro;
+  let combinedMacro =
+    macroParts.prefix + macroParts.combinedRoll + macroParts.combinedDamage;
 
   createMacroElement(macro, 'First Attack', 'firstAttack');
 
   if (haste && activeAction === 'fullRoundAttack') {
     createMacroElement(macro, 'Haste', 'hastedAttack');
-    macroRunning += macroParts.roll + macroParts.damage;
+    combinedMacro += macroParts.roll + macroParts.damage;
   }
 
   if (activeAction === 'fullRoundAttack') {
@@ -78,15 +79,15 @@ export function macroBuilder(activeAction, haste) {
     const macro5 = macroParts.prefix + macroParts.roll + macroParts.damage;
     console.log(macro5);
     createMacroElement(macro5, 'Second', 'secondAttack');
-    macroRunning += macroParts.combinedRoll + macroParts.damage;
+    combinedMacro += macroParts.combinedRoll + macroParts.combinedDamage;
 
     macroParts = macroComponents(-10, 2);
     const macro10 = macroParts.prefix + macroParts.roll + macroParts.damage;
     console.log(macro10);
     createMacroElement(macro10, 'Third', 'thirdAttack');
-    macroRunning += macroParts.combinedRoll + macroParts.damage;
+    combinedMacro += macroParts.combinedRoll + macroParts.combinedDamage;
 
-    createMacroElement(macroRunning, 'Combined', 'multiAttack');
+    createMacroElement(combinedMacro, 'Combined', 'multiAttack');
   }
 }
 
@@ -123,3 +124,9 @@ function calculateAttack() {
 
   mac.attackTotal = config.base.bab + config.base.strBonus + mac.attackBonus;
 }
+
+// reference - multiattack
+// &{template:pc}{{type=attackdamage}}{{name=FirstAttack}}{{attack=1}}{{showchar=[[1]]}}{{atkvs=(MeleevsAC)}}{{charname=LordGuber}}{{roll=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{critconfirm=[[1d20+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{rolldmg1=[[2d6+9]]}}{{rolldmg1type=Slashing}}{{rolldmg1crit=[[(2d6+9)*2]]}}{{roll1=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+-5[MAP]]]}}{{critconfirm1=[[1d20+14[BAB]+6[Strength]+0[Buff]+-5[MAP]]]}}{{roll1dmg1=[[2d6+9]]}}{{roll1dmg1type=Slashing}}{{roll1dmg1crit=[[(2d6+9)*2]]}}{{roll2=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+-10[MAP]]]}}{{critconfirm2=[[1d20+14[BAB]+6[Strength]+0[Buff]+-10[MAP]]]}}{{roll2dmg1=[[2d6+9]]}}{{roll2dmg1type=Slashing}}{{roll2dmg1crit=[[(2d6+9)*2]]}}
+
+// reference single attack
+// &{template:pc}{{type=attackdamage}}{{name=FirstAttack}}{{attack=1}}{{dmg1flag=1}}{{showchar=[[1]]}}{{atkvs=(MeleevsAC)}}{{charname=LordGuber}}{{roll=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{critconfirm=[[1d20+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{dmg1=[[2d6+9]]}}{{dmg1type=Slashing}}{{rolldmg1crit=[[(2d6+9)*2]]}}
