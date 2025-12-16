13764b41e60a3eabd51c9c022bb10c93a86bb55b
Dylan <rdylanmoore@gmail.com>
2025-12-15 18:12:01 -0600

    ready for merge

--- scripts/macro.js
+++ scripts/macro.js
@@ -1,7 +1,61 @@
 import * as config from './config.js';
 import { createMacroElement } from './dom.js';
 
+
+let macroParts = macroComponents(0, 0);
+
+const macro = macroParts.prefix + macroParts.roll + macroParts.damage;
+let combinedMacro =
+  macroParts.prefix + macroParts.combinedRoll + macroParts.combinedDamage;
+
+
+// ##################################################
+// ####            Calculate Bonuses             ####
+// ####     based on changes from state list     ####
+// ##################################################
+
+function calculateAttack() {
+  const dmg = config.damageBonuses;
+  const atk = config.attackBonuses;
+  const mac = config.macroDefaults;
+
+  // Damage Calculation
+  let untypedDamageBonus = dmg.untyped.reduce((acc, a) => acc + a, 0);
+  mac.damageBase = Math.floor(config.baseStats.strBonus * 1.5);
+  mac.damageBonus =
+    Math.max(...dmg.enhancement) +
+    Math.max(...dmg.luck) +
+    Math.max(...dmg.morale) +
+    Math.max(...dmg.item) +
+    Math.max(...dmg.profane) +
+    Math.max(...dmg.sacred) +
+    untypedDamageBonus;
+
+  mac.damageTotal = mac.damageBase + mac.damageBonus;
+
+  let untypedAttackBonus = atk.untyped.reduce((acc, a) => acc + a, 0);
+
+  mac.attackBonus =
+    Math.max(...atk.circumstance) +
+    Math.max(...atk.competence) +
+    Math.max(...atk.enhancement) +
+    Math.max(...atk.insight) +
+    Math.max(...atk.luck) +
+    Math.max(...atk.morale) +
+    Math.max(...atk.size) +
+    Math.max(...atk.item) +
+    untypedAttackBonus;
+
+  mac.attackTotal =
+    config.baseStats.bab + config.baseStats.strBonus + mac.attackBonus;
+}
+
+// ##################################################
+// ####         Build Macro Components           ####
+// ##################################################
+
 function macroComponents(map, attackNumber) {
+
   // ### Create Empty Object and calculate variables ###
   let macroParts = {};
   calculateAttack();
@@ -61,13 +115,9 @@ function macroComponents(map, attackNumber) {
   return macroParts;
 }
 
-let macroParts = macroComponents(0, 0);
-
-const macro = macroParts.prefix + macroParts.roll + macroParts.damage;
-let combinedMacro =
-  macroParts.prefix + macroParts.combinedRoll + macroParts.combinedDamage;
-
 export function macroBuilder(activeAction, haste) {
+
+  macroComponents(0, 1)
   createMacroElement(macro, 'First Attack', 'firstAttack');
 
   if (haste && activeAction === 'fullRoundAttack') {
@@ -96,40 +146,7 @@ function iterativeMacroBuilder(attackNum) {
   combinedMacro += macroParts.combinedRoll + macroParts.combinedDamage;
 }
 
-function calculateAttack() {
-  const dmg = config.damageBonuses;
-  const atk = config.attackBonuses;
-  const mac = config.macroDefaults;
 
-  let untypedDamageBonus = dmg.untyped.reduce((acc, a) => acc + a, 0);
-  mac.damageBase = Math.floor(config.baseStats.strBonus * 1.5);
-  mac.damageBonus =
-    Math.max(...dmg.enhancement) +
-    Math.max(...dmg.luck) +
-    Math.max(...dmg.morale) +
-    Math.max(...dmg.item) +
-    Math.max(...dmg.profane) +
-    Math.max(...dmg.sacred) +
-    untypedDamageBonus;
-
-  mac.damageTotal = mac.damageBase + mac.damageBonus;
-
-  let untypedAttackBonus = atk.untyped.reduce((acc, a) => acc + a, 0);
-
-  mac.attackBonus =
-    Math.max(...atk.circumstance) +
-    Math.max(...atk.competence) +
-    Math.max(...atk.enhancement) +
-    Math.max(...atk.insight) +
-    Math.max(...atk.luck) +
-    Math.max(...atk.morale) +
-    Math.max(...atk.size) +
-    Math.max(...atk.item) +
-    untypedAttackBonus;
-
-  mac.attackTotal =
-    config.baseStats.bab + config.baseStats.strBonus + mac.attackBonus;
-}
 
 // reference - multiattack
 // &{template:pc}{{type=attackdamage}}{{name=FirstAttack}}{{attack=1}}{{showchar=[[1]]}}{{atkvs=(MeleevsAC)}}{{charname=LordGuber}}{{roll=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{critconfirm=[[1d20+14[BAB]+6[Strength]+0[Buff]+0[MAP]]]}}{{rolldmg1=[[2d6+9]]}}{{rolldmg1type=Slashing}}{{rolldmg1crit=[[(2d6+9)*2]]}}{{roll1=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+-5[MAP]]]}}{{critconfirm1=[[1d20+14[BAB]+6[Strength]+0[Buff]+-5[MAP]]]}}{{roll1dmg1=[[2d6+9]]}}{{roll1dmg1type=Slashing}}{{roll1dmg1crit=[[(2d6+9)*2]]}}{{roll2=[[1d20cs>19+14[BAB]+6[Strength]+0[Buff]+-10[MAP]]]}}{{critconfirm2=[[1d20+14[BAB]+6[Strength]+0[Buff]+-10[MAP]]]}}{{roll2dmg1=[[2d6+9]]}}{{roll2dmg1type=Slashing}}{{roll2dmg1crit=[[(2d6+9)*2]]}}
